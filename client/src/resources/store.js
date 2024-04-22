39197b323be35762bfa6dcbc1566d4cfa2dc130d
n1999ck <millere9999@gmail.com>
2024-04-22 17:07:10 -0400

    started api calls

--- client/src/resources/store.js
+++ client/src/resources/store.js
@@ -368,7 +368,13 @@ export const useStore = create((set) => ({
   
   attack: async (playerId, attackName) => {
     try {
-      axios.get(paths.root + "/attack").then(function (response) {
+      axios({
+        method: "post",
+        url: paths.root + "/attack",
+        data: {
+        } 
+        })
+        .then(function (response) {
         console.log(response);
       });
     } catch (error) {
